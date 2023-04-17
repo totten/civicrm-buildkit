642dece4a11260f098bfe0a0958c7b48f3ba0b5f
Tim Otten <totten@civicrm.org>
2023-04-17 13:07:07 -0700

    nix/pins - Add pre-release of 23.05

---
+++ nix/pins/23.05.nix
@@ -0,0 +1,7 @@
+/**
+ * v23.05 pre-release
+ */
+fetchTarball {
+  url = "https://github.com/nixos/nixpkgs/archive/df04182ff2ade96766fcb3a7927961ee28580939.tar.gz";
+  # sha256 = "1wwwwwwyjhaa5pv20gbfbirvjq6i3m7pqrq2msf0g7cv44vijwgw";
+}
