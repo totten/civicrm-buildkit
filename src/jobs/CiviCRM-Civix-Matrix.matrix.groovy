e874077a459f3eaa0c092ba55c2623ce84e3e834
Tim Otten <totten@civicrm.org>
2024-03-15 00:04:15 -0700

    Move permutation list to yaml

--- src/jobs/CiviCRM-Civix-Matrix.matrix.groovy
+++ src/jobs/CiviCRM-Civix-Matrix.matrix.groovy
@@ -1,5 +1,7 @@
 /* Auto-generated from CiviCRM-Civix-Matrix.matrix.php via update-matrices.php */
 
+import groovy.yaml.YamlSlurper
+
 String signature(Map item, List keys) {
     def sig = ''
     keys.each { key ->
@@ -8,28 +10,11 @@ String signature(Map item, List keys) {
     return sig
 }
 
-def expectedItems = [
-  [CIVIVER: 'master', BLDTYPE: 'backdrop-clean', BKPROF: 'php74m57'],
-  [CIVIVER: 'master', BLDTYPE: 'backdrop-clean', BKPROF: 'php82m80'],
-  [CIVIVER: 'master', BLDTYPE: 'drupal-clean', BKPROF: 'php74m57'],
-  [CIVIVER: 'master', BLDTYPE: 'drupal-clean', BKPROF: 'php82m80'],
-  [CIVIVER: 'master', BLDTYPE: 'drupal-clean', BKPROF: 'php83m80'],
-  [CIVIVER: 'master', BLDTYPE: 'drupal9-clean', BKPROF: 'php74m57'],
-  [CIVIVER: 'master', BLDTYPE: 'drupal9-clean', BKPROF: 'php81m80'],
-  [CIVIVER: 'master', BLDTYPE: 'drupal10-clean', BKPROF: 'php81m57'],
-  [CIVIVER: 'master', BLDTYPE: 'drupal10-clean', BKPROF: 'php82m80'],
-  [CIVIVER: 'master', BLDTYPE: 'wp-demo', BKPROF: 'php74m57'],
-  [CIVIVER: 'master', BLDTYPE: 'wp-demo', BKPROF: 'php82m80'],
-  [CIVIVER: '5.69', BLDTYPE: 'backdrop-clean', BKPROF: 'php81m80'],
-  [CIVIVER: '5.69', BLDTYPE: 'drupal-clean', BKPROF: 'php73m57'],
-  [CIVIVER: '5.69', BLDTYPE: 'drupal9-clean', BKPROF: 'php74m57'],
-  [CIVIVER: '5.69', BLDTYPE: 'drupal10-clean', BKPROF: 'php81m80'],
-  [CIVIVER: '5.69', BLDTYPE: 'wp-demo', BKPROF: 'php73m57'],
-  [CIVIVER: '5.57', BLDTYPE: 'backdrop-clean', BKPROF: 'php81m80'],
-  [CIVIVER: '5.57', BLDTYPE: 'drupal-clean', BKPROF: 'php73m57'],
-  [CIVIVER: '5.57', BLDTYPE: 'drupal9-clean', BKPROF: 'php74m57'],
-  [CIVIVER: '5.57', BLDTYPE: 'wp-demo', BKPROF: 'php73m57'],
-]
+def yamlFilePath = "${WORKSPACE}/src/jobs/CiviCRM-Civix-Matrix.yaml"
+def yamlFile = new File(yamlFilePath)
+def yamlSlurper = new YamlSlurper()
+def yamlData = yamlSlurper.parseText(yamlFile.text)
+def expectedItems = yamlData.permutations
 
 def keys = expectedItems[0].keySet().toList()
 def expectedSignatures = expectedItems.collect { item ->
